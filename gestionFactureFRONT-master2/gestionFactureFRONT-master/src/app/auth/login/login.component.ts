e35ee785d31e0e6e611cba181a1884eeaa289ff5
asma-bargaoui <asma1bargaoui@gmail.com>
2023-06-02 10:44:35 +0100

    updated

---
+++ gestionFactureFRONT-master2/gestionFactureFRONT-master/src/app/auth/login/login.component.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup
+  errorMessage: string = '';
+
+  constructor(private loginService: LoginService, private fb: FormBuilder, private router: Router){}
+
+  ngOnInit(): void {
+    this.initForm()
+  }
+
+  initForm(){
+    this.loginForm = this.fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.required],
+    })
+  }
+
+ /**  login() {
+    this.loginService.logIn(this.loginForm.value).subscribe(
+      (data: any) => {
+        if (data.accessToken) {
+          localStorage.setItem("jwt", `${data.tokenType} ${data.accessToken}`);
+          localStorage.setItem("role", data.roles[0]);
+          this.router.navigate(['/home/']);
+        } else {
+          alert("Incorrect username or password");
+        }
+        console.log(data);
+      },
+      (error: any) => {
+        console.log("Login error:", error);
+        alert("Login error. Please try again.");
+      }
+    );
+  }*/
+  login() {
+    this.errorMessage = ''; // Clear previous error message
+    this.loginService.logIn(this.loginForm.value).subscribe(
+      (data: any) => {
+        if (data.accessToken) {
+          localStorage.setItem('jwt', `${data.tokenType} ${data.accessToken}`);
+          localStorage.setItem('role', data.roles[0]);
+          this.router.navigate(['/home']);
+        }
+      },
+      (error) => {
+        this.errorMessage = 'Invalid username or password'; // Set the error message
+        console.log(error);
+      }
+    );
+  }
+
+      
+}
+function user(arg0: never[]) {
+  throw new Error('Function not implemented.');
+}
+
+function manage_user(arg0: string[]) {
+  throw new Error('Function not implemented.');
+}
+
